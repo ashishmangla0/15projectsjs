734707a44987c7aa1bf71a4b4db382ba5e40b777
surbhirani <ashishmangla1991@gmail.com>
2020-06-19 07:37:26 +0530

    add clickcounter

---
+++ clickcounter/main.js
@@ -0,0 +1,31 @@
+let count = 0; //initial value
+const valueCont = document.querySelector('#value');
+const reset = document.querySelector('.reset');
+const btnContainer = document.querySelector('.btn-container');
+function buttonClick(value) {
+    let lowerValue = value.toLowerCase();
+    console.log(lowerValue)
+    switch (lowerValue) {
+        case "reset":
+            count = 0;
+            valueCont.innerHTML = count;
+
+            break
+        case "increase":
+            count += 1;
+            valueCont.innerHTML = count;
+            break;
+        case "decrease":
+            count -= 1;
+            valueCont.innerHTML = count;
+            break;
+    }
+}
+function init() {
+    btnContainer.addEventListener('click', function (e) {
+        buttonClick(e.target.innerText);
+    })
+}
+init()
+
+
